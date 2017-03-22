fa23620fec560d842c7ce9053eb95a2c5f40964d
OppaCoding <adun221@naver.com>
2017-03-22 17:12:06 +0900

    BinaryTree _ BASIC
    BinaryTree _ BASIC

---
+++ BinaryTree.h
@@ -0,0 +1,24 @@
+#ifndef __BINARY_TREE_H__
+#define __BINARY_TREE_H__
+
+typedef int BTData;
+
+typedef struct _bTreeNode
+{
+    BTData data;
+    struct _bTreeNode* left;
+    struct _bTreeNode* right;
+
+} BTreeNode;
+
+BTreeNode* MakeBTreeNode(void);                        // 노드 생성
+BTData GetData(BTreeNode* bt);                         // 노드에 저장된 데이터를 반환
+void SetData(BTreeNode* bt, BTData data);              // 노드에 데이터를 저장
+
+BTreeNode* GetLeftSubTree(BTreeNode* bt);              // 왼쪽  서브 트리 주소 값 반환
+BTreeNode* GetRightSubTree(BTreeNode* bt);             // 오른쪽 서브 트리 주소 값 반환
+
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub);  // 노드의 왼쪽 자식 노드로 연결
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub); // 노드의 오른쪽 자식 노드로 연결
+
+#endif
