12be40f40eaf735aeb239f86b99d4ad4e0883e43
OppaCoding <adun221@naver.com>
2017-05-28 04:11:05 +0900

    HeapSort

--- HeapSort.c
+++ HeapSort.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include "UsefulHeap.h"
+
+int PriComp(int n1, int n2)
+{
+    return n2 - n1;   // 오름차순 정렬
+    // return n1 - n2;
+}
+
+void HeapSort(int arr[], int n, PriorityComp pc)
+{
+    Heap heap;
+    HeapInit(&heap, pc);
+
+    for(int i = 0; i < n; ++i)
+        HInsert(&heap, arr[i]);
+
+    for(int i = 0; i < n; ++i)
+        arr[i] = HDelete(&heap);
+}
+
+int main(void)
+{
+    int arr[4] = {3, 4, 2, 1};
+
+    HeapSort(arr, sizeof(arr)/sizeof(int), PriComp);
+
+    for(int i = 0; i < 4; ++i)
+        printf("%d", arr[i]);
+
+    return 1;
+}
